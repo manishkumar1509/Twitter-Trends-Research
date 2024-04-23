da93b603c138da02d829d5b7b1d9049814b636bd
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2024-04-22 21:31:54 -0700

    Add code for US, worldwide, pakistan and UK

--- screenshots.py
+++ screenshots.py
@@ -17,16 +17,26 @@ def take_screenshot(url, output_file):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     driver.set_window_size(1500, 1200)
-    #driver.maximize_window()
+
     # Wait for the page to load (you can adjust the sleep time as needed)
     time.sleep(3)
-    print('starting screenshot')
     driver.get_screenshot_as_file(output_file)                                                                                                          
-
-    print('screenshot taken')
     driver.quit()
 
 if __name__ == "__main__":
     timestamp = datetime.datetime.now()
+
     output_file_name = 'results/India/screenshot_'+ str(timestamp) + '.png'
     take_screenshot("https://getdaytrends.com/india/", output_file_name)
+
+    output_file_name = 'results/US/screenshot_'+ str(timestamp) + '.png'
+    take_screenshot("https://getdaytrends.com/united-states/", output_file_name)
+
+    output_file_name = 'results/India/worldwide'+ str(timestamp) + '.png'
+    take_screenshot("https://getdaytrends.com/", output_file_name)
+
+    output_file_name = 'results/Pakistan/screenshot_'+ str(timestamp) + '.png'
+    take_screenshot("https://getdaytrends.com/pakistan/", output_file_name)
+
+    output_file_name = 'results/UK/screenshot_'+ str(timestamp) + '.png'
+    take_screenshot("https://getdaytrends.com/united-kingdom/", output_file_name)
